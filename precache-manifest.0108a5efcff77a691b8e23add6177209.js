9aa806d72ebd3d9e6f2977d35631e3e3c855fcb7
liran <liran9800@gmail.com>
2023-01-14 07:14:03 +0200

    Updates

---
+++ precache-manifest.0108a5efcff77a691b8e23add6177209.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a676178d9c7398d81ad001733b4bbe86",
+    "url": "/products-frontend/index.html"
+  },
+  {
+    "revision": "f456b461d0f32bf201cc",
+    "url": "/products-frontend/static/css/main.8d70a6e7.chunk.css"
+  },
+  {
+    "revision": "1538ad9c9340396c94bb",
+    "url": "/products-frontend/static/js/2.998ac1d7.chunk.js"
+  },
+  {
+    "revision": "f456b461d0f32bf201cc",
+    "url": "/products-frontend/static/js/main.d26e48d8.chunk.js"
+  },
+  {
+    "revision": "a2939c52d2e09a8a9e15",
+    "url": "/products-frontend/static/js/runtime-main.9eaf67f1.js"
+  }
+]);
